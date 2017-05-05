1cf184b0780e3b62d73c6ae67279b394e5a67245
baiyidjp <271524878@qq.com>
2017-05-05 09:57:32 +0800

    Initial Commit

---
+++ Password/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Password
+//
+//  Created by Keep丶Dream on 2017/5/5.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Password/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Password
+//
+//  Created by Keep丶Dream on 2017/5/5.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
